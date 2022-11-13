86db6fe276d208f70d16aed382749d9a1f5f0221
JuanPablo2655 <personal@isidrocasiano.com>
2022-11-13 15:35:12 -0500

    feat: wip blogs

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 'use strict';
 // Projects
 const projects = document.querySelector('.projects');
-console.log(projects);
+// console.log(projects);
 
-const result = async () => await (await fetch('./data/projects.json')).json();
-result().then(data => {
+const _projects = async () => await (await fetch('./data/projects.json')).json();
+_projects().then(data => {
 	data.sort((a, b) => b.stars - a.stars).forEach(d => addProject(d));
 });
 
@@ -23,3 +23,32 @@ function addProject(d) {
 	div.append(h3, p);
 	projects.append(project);
 }
+
+// Blogs
+
+const blogs = document.querySelector('.main-blogs');
+console.log(blogs);
+const _blogs = async () => await (await fetch('./data/blogs.json')).json();
+_blogs().then(data => {
+	const years = [...new Set(data.map(d => d.date.split('-')[0]))].sort((a, b) => b - a);
+	console.log(years);
+	years.forEach(y => {
+		addYear(y);
+		data.filter(d => d.date.split('-')[0] === y).forEach(d => addBlog(d));
+	});
+	// data.sort((a, b) => new Date(b.date) - new Date(a.date)).forEach(d => addBlog(d));
+});
+
+function addYear(y) {
+	const year = document.createElement('div');
+	year.classList.add('blog-year');
+	blogs.append(year);
+	const h2 = document.createElement('h2');
+	h2.textContent = y;
+	year.append(h2);
+	const blogs = document.createElement('div');
+	blogs.classList.add('blogs');
+	year.append(blogs);
+}
+
+function addBlog(d) {}
